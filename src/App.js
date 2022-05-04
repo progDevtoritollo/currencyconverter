800013baaa2540236802866494a9a7f7057aa24d
Ihor <progdevtoritollo@gmail.com>
2022-05-04 19:25:20 +0300

    succsess respones

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import "./App.css";
+import CurrencyInput from "./CurrencyInput";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const URL_API =
+    "https://api.apilayer.com/fixer/latest?apikey=GfALTWcSomk8apHZcbJAXR92aUWNnEIM";
+
+  const [amount1, setAmount1] = useState(1);
+  const [amount2, setAmount2] = useState(1);
+  const [currency1, setcurrency1] = useState("USD");
+  const [currency2, setcurrency2] = useState("UAN");
+
+  useEffect(() => {
+    axios.get(URL_API).then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CurrencyInput
+        currencies={["USD"]}
+        amount={amount1}
+        currency={currency1}
+      />
+      <CurrencyInput
+        currencies={["USD"]}
+        amount={amount2}
+        currency={currency2}
+      />
     </div>
   );
 }
